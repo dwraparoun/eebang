9820fe1c40eecab6b1892b53727fdd948a487c61
Dmytro Bondal <dmytrobondal@Dmytros-MacBook-Air.local>
2024-01-01 23:08:28 +0100

    Add derivative.py

--- derivative.py
+++ derivative.py
@@ -8,7 +8,14 @@ parser.add_argument(
     type=str,
     help="single variable function, e.g. 'x**3-7+sin(x)-1/exp(4*x)'",
 )
+parser.add_argument("-n", type=int, default=1, help="derivative order")
+parser.add_argument("--verbose", "-v", action="store_true")
 args = parser.parse_args()
 x_sym, f_sym = symparser.parse_single_variable_function(args.function)
-p = sy.plotting.plot(f_sym, show=False)
-p.show()
+fprime_sym = f_sym
+for i in range(abs(args.n)):
+    if args.verbose:
+        print(fprime_sym)
+    fprime_sym = fprime_sym.diff(x_sym)
+
+print(fprime_sym)
